f79484a3391b13550474517904c560a94d6718b6
Chad <cdhqwebdev@gmail.com>
2017-11-30 17:49:29 -0500

    edit book list reducer

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -3,6 +3,9 @@ export default function () {
     {title:"JS the good stuff", pages: 207 },
     {title:"Frog Pajamas", pages: 500 },
     {title:"Lunar Park", pages: 304 },
+    {title:"Nikola Tesla: The Genius", pages: 1000 },
+    {title:"Redux for Beginners", pages: 48 },
+    {title:"Coding for Dummies", pages: 23 },
     {title:"JS lifestyle", pages: 10 }
   ]
 }
